332ac57c20c47cae6781687205e6cc06897c1d82
vayebantu <48871658+vayebantu@users.noreply.github.com>
2025-05-23 14:50:59 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('my-button')
+
+button.addEventListener('click', doSomething)
+
+function doSomething(){
+    alert('Are you sure you want to submit this form?')
+}
+
+
